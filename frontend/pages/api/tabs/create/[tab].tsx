df0da3af7d7a5ca1ee55db82f86d0ea8abad160c
felix <mrKaasa@protonmail.com>
2022-12-07 09:26:59 +0100

    fixed a bug where i need an empty object to post

--- frontend/pages/api/tabs/create/[tab].tsx
+++ frontend/pages/api/tabs/create/[tab].tsx
@@ -5,24 +5,24 @@ import { getSession } from "next-auth/react";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req });
   const { tab } = req.query;
+  const url = "http://0.0.0.0:8080/tab/" + tab;
   if (session) {
+    console.log(session.user.backendToken)
     const config: AxiosRequestConfig = {
       method: "POST",
       headers: { Authorization: `Bearer ${session.user.backendToken}` },
     };
 
     return await axios
-      .post(
-        "http://0.0.0.0:8080/tab" + tab,
-        config
-      )
+      .post(url, {},config)
       .then((response) => {
         console.log(response);
         res.status(response.status).json({ message: "server response ok" });
       })
       .catch((err) => {
-        console.log(err);
-        res.status(400).json(err); //{ message: "server response error" });
+        //console.log(err);
+        console.log(err.status);
+        res.status(400).json(err);
       });
   }
 };
